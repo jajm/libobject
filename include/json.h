9f3c659b3031b754f323b4adf49e6a9799c1a289
Julian Maurice <jajm@jajm.org>
2013-08-26 16:23:48 +0200

    Fix json -> object and complete object -> json

--- include/json.h
+++ include/json.h
@@ -13,9 +13,17 @@ object_from_json_file(
 	const char *filename
 );
 
+#define OBJECT_TO_JSON_PLAIN 0
+#define OBJECT_TO_JSON_SPACED (1 << 0)
+#define OBJECT_TO_JSON_PRETTY (1 << 1)
+#define OBJECT_TO_JSON_NOZERO (1 << 2)
+#define OBJECT_TO_JSON_VERY_PRETTY \
+	(OBJECT_TO_JSON_SPACED|OBJECT_TO_JSON_PRETTY|OBJECT_TO_JSON_NOZERO)
+
 char *
 object_to_json(
-	object_t *object
+	object_t *object,
+	int flags
 );
 
 #endif /* ! object_json_h_included */
--- src/json.c
+++ src/json.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <json-c/json.h>
+#include "malloc.h"
 #include "json.h"
 #include "object.h"
 #include "hash.h"
 #include "array.h"
+#include "array_iterator.h"
 #include "integer.h"
 #include "real.h"
 #include "boolean.h"
 
-object_t * json_object_to_object(struct json_object *json_object);
+object_t * object_from_json_object(struct json_object *json_object);
 
-hash_t * json_object_to_hash(struct json_object *json_object)
+hash_t * hash_from_json_object(struct json_object *json_object)
 {
 	struct json_object_iterator it, it_end;
 	hash_t *hash;
@@ -20,20 +22,18 @@ hash_t * json_object_to_hash(struct json_object *json_object)
 	it = json_object_iter_begin(json_object);
 	it_end = json_object_iter_end(json_object);
 	hash = hash_new();
-	printf("HASH\n");
 
 	while (!json_object_iter_equal(&it, &it_end)) {
 		const char *name = json_object_iter_peek_name(&it);
-		printf("%s => \n", name);
 		struct json_object *value = json_object_iter_peek_value(&it);
-		hash_set(hash, name, json_object_to_object(value));
+		hash_set(hash, name, object_from_json_object(value));
 		json_object_iter_next(&it);
 	}
 
 	return hash;
 }
 
-array_t * json_object_to_array(struct json_object *json_object)
+array_t * array_from_json_object(struct json_object *json_object)
 {
 	array_t *array;
 	struct array_list *array_list;
@@ -41,32 +41,30 @@ array_t * json_object_to_array(struct json_object *json_object)
 	int idx;
 
 	array = array();
-	printf("ARRAY\n");
 	array_list = json_object_get_array(json_object);
 	length = array_list_length(array_list);
 	idx = 0;
 	while (idx < length) {
 		struct json_object *value = array_list_get_idx(array_list, idx);
-		array_push(array, json_object_to_object(value));
+		array_push(array, object_from_json_object(value));
 		idx++;
 	}
 
-	return array();
+	return array;
 }
 
-string_t * json_object_to_string(struct json_object *json_object)
+string_t * string_from_json_object(struct json_object *json_object)
 {
 	string_t *string;
 	const char *value;
 
 	value = json_object_get_string(json_object);
-	printf("STRING: %s\n", value);
-	string = string_new(value);
+	string = string(value);
 
 	return string;
 }
 
-object_t * json_object_to_object(struct json_object *json_object)
+object_t * object_from_json_object(struct json_object *json_object)
 {
 	object_t *object;
 	enum json_type type;
@@ -74,7 +72,7 @@ object_t * json_object_to_object(struct json_object *json_object)
 	type = json_object_get_type(json_object);
 	switch (type) {
 		case json_type_null:
-			object = object_new("", NULL);
+			object = NULL;
 			break;
 		case json_type_boolean:
 			object = boolean_new(json_object_get_boolean(json_object));
@@ -86,13 +84,13 @@ object_t * json_object_to_object(struct json_object *json_object)
 			object = integer_new(json_object_get_int(json_object));
 			break;
 		case json_type_object:
-			object = json_object_to_hash(json_object);
+			object = hash_from_json_object(json_object);
 			break;
 		case json_type_array:
-			object = json_object_to_array(json_object);
+			object = array_from_json_object(json_object);
 			break;
 		case json_type_string:
-			object = json_object_to_string(json_object);
+			object = string_from_json_object(json_object);
 			break;
 	}
 
@@ -105,7 +103,8 @@ object_t * object_from_json(const char *json)
 	object_t *object;
 
 	json_object = json_tokener_parse(json);
-	object = json_object_to_object(json_object);
+	object = object_from_json_object(json_object);
+	json_object_put(json_object);
 
 	return object;
 }
@@ -116,22 +115,60 @@ object_t * object_from_json_file(const char *filename)
 	object_t *object;
 
 	json_object = json_object_from_file(filename);
-	object = json_object_to_object(json_object);
+	object = object_from_json_object(json_object);
+	json_object_put(json_object);
 
 	return object;
 }
 
-char * object_to_json(object_t *object)
+struct json_object * object_to_json_object(object_t *object)
+{
+	struct json_object *json_object;
+
+	if (!object_isset(object)) {
+		json_object = NULL;
+	} else if (object_is_boolean(object)) {
+		json_object = json_object_new_boolean(boolean_get(object));
+	} else if (object_is_real(object)) {
+		json_object = json_object_new_double(real_get(object));
+	} else if (object_is_integer(object)) {
+		json_object = json_object_new_int(integer_get(object));
+	} else if (object_is_hash(object)) {
+		json_object = json_object_new_object();
+		array_t *keys = hash_keys(object);
+		array_foreach(keys, key) {
+			const char *k = string_to_c_str(key);
+			object_t *value = hash_get(object, k);
+			struct json_object *jo = object_to_json_object(value);
+			json_object_object_add(json_object, k, jo);
+		}
+		array_free(keys);
+	} else if (object_is_array(object)) {
+		json_object = json_object_new_array();
+		array_foreach(object, value) {
+			struct json_object *jo = object_to_json_object(value);
+			json_object_array_add(json_object, jo);
+		}
+	} else if (object_is_string(object)) {
+		json_object = json_object_new_string(string_to_c_str(object));
+	}
+
+	return json_object;
+}
+
+char * object_to_json(object_t *object, int flags)
 {
 	struct json_object *json_object = NULL;
 	const char *json_string;
 	char *json;
 
-	// TODO Create json_object
-	if (object) {}
-	json_string = json_object_to_json_string(json_object);
-	json = malloc(sizeof(char) * (strlen(json_string) + 1));
-	strcpy(json, json_string);
+	if (object) {
+		json_object = object_to_json_object(object);
+		json_string = json_object_to_json_string_ext(json_object, flags);
+		json = object_malloc(sizeof(char) * (strlen(json_string) + 1));
+		strcpy(json, json_string);
+		json_object_put(json_object);
+	}
 
 	return json;
 }
