9562398bdc1ceca1e3ffe8ccb3c15903ea15ba71
Julian Maurice <julian.maurice@biblibre.com>
2013-11-06 17:39:34 +0100

    Add documentation to include/hash.h

--- include/hash.h
+++ include/hash.h
@@ -31,9 +31,23 @@ typedef object_t hash_t;
 extern "C" {
 #endif
 
+/* Create a new hash.
+ *
+ * Returns
+ *   Pointer to hash.
+ */
 hash_t *
 hash_new(void);
 
+/* Create a new hash from an array of objects.
+ *
+ * Parameters
+ *   n       : Size of array.
+ *   objects : Array of objects.
+ *
+ * Returns
+ *   Pointer to hash.
+ */
 hash_t *
 hash_new_from_array(
 	unsigned int n,
@@ -45,6 +59,20 @@ hash_new_from_array(
 	hash_new_from_array(sizeof(__args) / sizeof(*__args), __args); \
 })
 
+/* Set/Add a value in the hash.
+ *
+ * If key is already present in the hash, associated object is freed before
+ * changing the value.
+ *
+ * Parameters
+ *   hash  : Pointer to hash.
+ *   key   : Key associated to value.
+ *   value : Value to insert into hash.
+ *
+ * Returns
+ *   0 if key was just added.
+ *   1 if key was already in the hash.
+ */
 int
 hash_set(
 	hash_t *hash,
@@ -52,33 +80,84 @@ hash_set(
 	object_t *value
 );
 
+/* Unset a value from the hash.
+ *
+ * Parameters
+ *   hash : Pointer to hash.
+ *   key  : Key to unset.
+ *
+ * Returns
+ *   0 if key was correctly unset.
+ *   1 if key was not in the hash.
+ */
 int
 hash_unset(
 	hash_t *hash,
 	const char *key
 );
 
+/* Retrieve a value from the hash.
+ *
+ * Parameters
+ *   hash : Pointer to hash.
+ *   key  : Key of value to retrieve.
+ *
+ * Returns
+ *   Pointer to value.
+ */
 object_t *
 hash_get(
 	const hash_t *hash,
 	const char *key
 );
 
+/* Return list of keys present in hash.
+ *
+ * Parameters
+ *   hash : Pointer to hash.
+ *
+ * Returns
+ *   an array containing string_t objects representing the keys in the hash.
+ */
 array_t *
 hash_keys(
 	const hash_t *hash
 );
 
+/* Return list of values present in hash.
+ *
+ * Parameters
+ *   hash : Pointer to hash.
+ *
+ * Returns
+ *   an array containing objects representing the values in the hash.
+ */
 array_t *
 hash_values(
 	const hash_t *hash
 );
 
+/* Free memory used by hash.
+ *
+ * Deprecated. Use object_free instead.
+ *
+ * Parameters
+ *   hash : Pointer to hash.
+ */
 void
 hash_free(
 	hash_t *hash
 );
 
+/* Tell if object is a hash.
+ *
+ * Parameters
+ *   object : Pointer to object.
+ *
+ * Returns
+ *   0 if object is not a hash.
+ *   1 if object is a hash.
+ */
 int
 object_is_hash(
 	const object_t *object
